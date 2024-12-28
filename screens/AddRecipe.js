c21f948640ccf308ca14484462272cd940ac41d0
SafiaKhalid <safia.am.khalid@gmail.com>
2024-12-28 15:43:39 +0000

    Added onChange functions to change newRecipe state when text inputted in AddRecipe screen

--- screens/AddRecipe.js
+++ screens/AddRecipe.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 const AddRecipe = () => {
@@ -11,19 +11,55 @@ const AddRecipe = () => {
         notes: undefined,
     });
 
+    useEffect(() => {
+        console.log('newRecipe: ', newRecipe);
+    }, [newRecipe]);
+
     return (
         <View>
             <Text>Add Recipe</Text>
-            <TextInput placeholder="name" />
-            <TextInput placeholder="prep_time" />
-            <TextInput placeholder="cook_time" />
-            <TextInput placeholder="servings" />
+            <TextInput
+                placeholder="name"
+                onChangeText={(e) => setNewRecipe({ ...newRecipe, name: e })}
+                value={newRecipe.name}
+            />
+            <TextInput
+                placeholder="prep_time"
+                onChangeText={(e) =>
+                    setNewRecipe({ ...newRecipe, prep_time: e })
+                }
+                value={newRecipe.prep_time}
+            />
+            <TextInput
+                placeholder="cook_time"
+                onChangeText={(e) =>
+                    setNewRecipe({ ...newRecipe, cook_time: e })
+                }
+                value={newRecipe.cook_time}
+            />
+            <TextInput
+                placeholder="servings"
+                onChangeText={(e) =>
+                    setNewRecipe({ ...newRecipe, servings: e })
+                }
+                value={newRecipe.servings}
+            />
             <TextInput
                 placeholder="description"
+                onChangeText={(e) =>
+                    setNewRecipe({ ...newRecipe, description: e })
+                }
+                value={newRecipe.description}
                 multiline={true}
                 numberOfLines={4}
             />
-            <TextInput placeholder="notes" multiline={true} numberOfLines={4} />
+            <TextInput
+                placeholder="notes"
+                onChangeText={(e) => setNewRecipe({ ...newRecipe, notes: e })}
+                value={newRecipe.notes}
+                multiline={true}
+                numberOfLines={4}
+            />
         </View>
     );
 };
