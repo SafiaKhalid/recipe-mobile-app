230d47d75952333188497509f204dc34a74a19bd
SafiaKhalid <safia.am.khalid@gmail.com>
2024-12-28 15:14:00 +0000

    Added add recipe button to Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const Home = () => {
+    const addRecipe = () => {
+        console.log('add recipe');
+    };
+
     return (
         <View>
-            <Text>Home screen</Text>
+            <Text>My Recipes</Text>
+            <Text>No recipes added (yet)</Text>
+            <Button
+                onPress={addRecipe}
+                title="Add your first recipe"
+                accessibilityLabel="Click to add your first recipe"
+            />
         </View>
     );
 };
