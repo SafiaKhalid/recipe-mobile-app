03534454d732f6b953130fade7b8c3d587adf43b
SafiaKhalid <safia.am.khalid@gmail.com>
2024-12-28 15:38:08 +0000

    Added name, prep and cook time, servings, description and notes to newRecipe state in AddRecipe screen, added corresponting TextInput components

--- screens/AddRecipe.js
+++ screens/AddRecipe.js
@@ -1,9 +1,29 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 const AddRecipe = () => {
+    const [newRecipe, setNewRecipe] = useState({
+        name: undefined,
+        prep_time: null,
+        cook_time: null,
+        servings: null,
+        description: undefined,
+        notes: undefined,
+    });
+
     return (
         <View>
             <Text>Add Recipe</Text>
+            <TextInput placeholder="name" />
+            <TextInput placeholder="prep_time" />
+            <TextInput placeholder="cook_time" />
+            <TextInput placeholder="servings" />
+            <TextInput
+                placeholder="description"
+                multiline={true}
+                numberOfLines={4}
+            />
+            <TextInput placeholder="notes" multiline={true} numberOfLines={4} />
         </View>
     );
 };
