fdaf1831328b45b0f90a6e459265431df904d4f0
SafiaKhalid <safia.am.khalid@gmail.com>
2025-01-11 15:03:49 +0000

    Created ViewRecipe screen

---
+++ screens/ViewRecipe.js
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+const ViewRecipe = () => {
+    return (
+        <View>
+            <Text>View Recipe</Text>
+        </View>
+    );
+};
+
+export default ViewRecipe;
