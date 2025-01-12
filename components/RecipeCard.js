1b04bb876cacbe1141236aeafc3d804c083c5e5f
SafiaKhalid <safia.am.khalid@gmail.com>
2025-01-12 16:05:30 +0000

    Uninstalled react-native-fontawesome

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
 import { useGlobalContext } from '../context';
 
 const RecipeCard = ({ recipe }) => {
