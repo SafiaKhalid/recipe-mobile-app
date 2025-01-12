65164bb534bc191d3a3c5cd3f28a774fe9005dd1
SafiaKhalid <safia.am.khalid@gmail.com>
2025-01-12 16:27:29 +0000

    Added sort dropdown to Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, View, Text, Button, FlatList } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 import { useGlobalContext } from '../context';
 import RecipeCard from '../components/RecipeCard';
@@ -8,6 +9,18 @@ import RecipeCard from '../components/RecipeCard';
 const Home = () => {
     const { loading, initDB, recipes, clearDB } = useGlobalContext();
     const navigation = useNavigation();
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        { label: 'Date (newest)', value: 'dateDes' },
+        { label: 'Date (oldest)', value: 'dateAsc' },
+        { label: 'A - Z', value: 'aToZ' },
+        { label: 'Z - A', value: 'zToA' },
+        { label: 'Time (most)', value: 'timeDes' },
+        { label: 'Time (least)', value: 'timeAsc' },
+    ]);
+
+    const recipeList = [...recipes];
 
     useEffect(() => {
         initDB();
@@ -38,8 +51,17 @@ const Home = () => {
                 title="Add your first recipe"
                 accessibilityLabel="Click to add your first recipe"
             />
+            <Text>Sort by:</Text>
+            <DropDownPicker
+                open={open}
+                value={value}
+                items={items}
+                setOpen={setOpen}
+                setValue={setValue}
+                setItems={setItems}
+            />
             <FlatList
-                data={recipes}
+                data={recipeList}
                 renderItem={({ item }) => <RecipeCard recipe={item} />}
                 keyExtractor={(item) => item.id}
             />
